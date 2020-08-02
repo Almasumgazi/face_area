3e59b30cdd96600773b6821ec32e456983e1db78
Almasumgazi <almasumgazi18@gmail.com>
2020-08-02 14:46:13 +0530

    pushing files

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier('for_face.xml')
+cap = cv2.VideoCapture('v.mp4')
+
+
+while True:
+    _, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    faces = face_cascade.detectMultiScale(gray, 1.9, 6)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 1)
+
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(2) & 0xff
+    if k==27:
+        break
+        
+cap.release()
